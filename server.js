c4520f2a9cef6992e406c41a5746974a3981172e
engineering0999 <168620473+engineering0999@users.noreply.github.com>
2024-11-10 19:26:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 
 const express = require('express');
 const axios = require('axios');
-
+const cors = require('cors');
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 const pipedApiEndpoints = [
     "https://pipedapi.adminforge.de",
